f4cb986d68316ead18c477a44e727862e80c2f0f
Aaron Watters <awatters@flatironinstitute.org>
2024-05-03 13:51:10 -0400

    javascript infrastructure

---
+++ index.js
@@ -0,0 +1,10 @@
+
+// Dummy javascript index page for package.json
+// This file is not used in the project
+
+// NPM is used to manage Javascript packages and dependencies.
+// The Javascript is frozen in the Python package.
+// To load the latest Javascript, use the following command:
+
+// rm -r package-lock.json node_modules
+// npm install
